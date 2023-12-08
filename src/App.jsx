ada8ea7da1da1f4488d5b1ed486bb26a71035111
Usman Ghori <121635866+SoftwareSuite@users.noreply.github.com>
2023-12-08 10:16:53 -0500

    this is my new project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from 'react'
+import './App.css'
+import Header from './Componets/Header'
+import Home from './Pages/Home'
+import { ThemeContext } from './Context/ThemeContext'
+// import ThemeContext from './Context/ThemeContext'
+//export const ThemeContext = createContext('dark');
+
+function App() {
+    const [theme, setTheme] = useState('light')
+useEffect(()=>{
+   
+  setTheme(localStorage.getItem('theme')?localStorage.getItem('theme'):'dark')
+
+},[])
+  return (
+    <ThemeContext.Provider value={{theme,setTheme}}>
+    <div className={`${theme} ${theme=='dark'?'bg-[#121212]':null}`}>
+        <Header/>
+        <Home/>
+    </div>
+     </ThemeContext.Provider>
+  )
+}
+
+export default App
